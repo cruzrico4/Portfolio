9ea1a80fb98d85e924b677b2b84417263456bc37
Cruz <jcrico@ucsd.edu>
2020-03-10 16:32:53 -0700

    Even more updated paths

--- js/portfolio.js
+++ js/portfolio.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
         }
     });
     if(location.pathname.split("/").slice(-1) == "index.html")
-      document.getElementById("top_arrow").src = "images/top_arrow.png";
+      document.getElementById("top_arrow").src = "../images/top_arrow.png";
     else {
       document.getElementById("top_arrow").src = "../images/top_arrow.png";
     }
