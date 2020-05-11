401a431e7fc52f422d9415d356fd1461889d0073
Cruz <jcrico@ucsd.edu>
2020-05-10 21:17:21 -0700

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,15 +1,16 @@
 //Establish connection to Firebase
 
 $(document).ready(function(){
-var firebaseConfig = {
-  apiKey: "AIzaSyBMNFSf5TUGwsZhf4FBbUWmMV3LLmj4n4A",
-  authDomain: "website-hit-logger.firebaseapp.com",
-  databaseURL: "https://website-hit-logger.firebaseio.com",
-  projectId: "website-hit-logger",
-  storageBucket: "website-hit-logger.appspot.com",
-  messagingSenderId: "1085379599357",
-  appId: "1:1085379599357:web:bd3f2a199177564b091794",
-  measurementId: "G-measurement-id",
+  var firebaseConfig = {
+    apiKey: "AIzaSyBMNFSf5TUGwsZhf4FBbUWmMV3LLmj4n4A",
+    authDomain: "website-hit-logger.firebaseapp.com",
+    databaseURL: "https://website-hit-logger.firebaseio.com",
+    projectId: "website-hit-logger",
+    storageBucket: "website-hit-logger.appspot.com",
+    messagingSenderId: "1085379599357",
+    appId: "1:1085379599357:web:bd3f2a199177564b091794",
+    measurementId: "G-measurement-id",
+  }
 });
 
 
